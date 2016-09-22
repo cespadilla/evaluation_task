f445996d72451855241196d95a6faec7ec30c9b9
joel espadilla <jespadilla0128@gmail.com>
2016-09-22 18:09:46 +0800

    update content page

--- application/views/admin/content_edit.php
+++ application/views/admin/content_edit.php
@@ -3,6 +3,17 @@
     <div class="row">
         <div class="col-lg-12">
             <?php echo form_open();?>
+            <div class="form-group">
+                <?php
+
+                echo validation_errors();
+                ?>
+                <?php
+                echo form_label('Text','text_field');
+                echo form_error('text_field');
+                echo form_textarea('text_field',null,'class="form-control"');
+                ?>
+            </div>
             
             <?php echo form_submit('submit', "test", 'class="btn btn-primary btn-lg btn-block"');?>
             <?php echo form_close();?>
